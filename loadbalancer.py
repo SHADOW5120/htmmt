1ebb912a7dca6fe30d2c33fb8c1824afad0ce54b
SHADOW5120 <162798830+SHADOW5120@users.noreply.github.com>
2024-11-12 17:54:29 +0700

    fix2

--- loadbalancer.py
+++ loadbalancer.py
@@ -32,9 +32,14 @@ def deviceInformation(data):
 	global hostPorts
 	switchDPID = ""
 	for i in data:
-		if(i['ipv4']):
-			ip = i['ipv4'][0].encode('ascii','ignore') # The encode method is unnecessary for strings in Python 3 unless you need to work explicitly with bytes. Strings are already Unicode by default.
-			mac = i['mac'][0].encode('ascii','ignore')
+		ipv4_list = i.get('ipv4', [])
+		mac_list = i.get('mac', [])
+
+		if ipv4_list and mac_list:
+			# Proceed with your logic if both lists are non-empty
+			# For example:
+			ip = ipv4_list[0]
+			mac = mac_list[0]
 			deviceMAC[ip] = mac
 			for j in i['attachmentPoint']:
 				for key in j:
